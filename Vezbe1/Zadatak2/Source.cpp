f01b5bd53ec29bc85a2ef56375f3107d13813e93
Dragan <milosevicdragan002@gmail.com>
2023-10-21 09:46:04 +0200

    WSAStartup

--- Vezbe1/Zadatak2/Source.cpp
+++ Vezbe1/Zadatak2/Source.cpp
@@ -1,1 +1,30 @@
-#define _WINSOCK_DEPRECATED_NO_WARNINGS#define WIN32_LEAN_AND_MEAN#include <Windows.h>#include <winsock2.h>#include <WS2tcpip.h>#include <stdlib.h>#include <stdio.h>#include <conio.h>#pragma comment(lib,"Ws2_32.lib")#pragma comment(lib,"Mswsock.lib")#pragma comment(lib,"AdvApi32.lib")int main(){	return 0;}
+#define _WINSOCK_DEPRECATED_NO_WARNINGS 
+
+#define WIN32_LEAN_AND_MEAN
+
+#include <Windows.h>
+#include <winsock2.h>
+#include <WS2tcpip.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "conio.h"
+
+#pragma comment(lib,"Ws2_32.lib")
+#pragma comment(lib,"Mswsock.lib")
+#pragma comment(lib,"AdvApi32.lib")
+
+int main()
+{
+	WSADATA wsaData;
+	// Initialize windows sockets library for this process
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+	{
+		printf("WSAStartup failed with error: %d\n", WSAGetLastError());
+		return 1;
+	}
+
+
+
+	return 0;
+}
+
