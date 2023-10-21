6d3200f4f619833a47ee3ce704936e842dc44c48
Dragan <milosevicdragan002@gmail.com>
2023-10-21 09:35:23 +0200

    Include all necessary libraries

---
+++ Vezbe1/Zadatak2/Source.cpp
@@ -0,0 +1,1 @@
+#define _WINSOCK_DEPRECATED_NO_WARNINGS#define WIN32_LEAN_AND_MEAN#include <Windows.h>#include <winsock2.h>#include <WS2tcpip.h>#include <stdlib.h>#include <stdio.h>#include <conio.h>#pragma comment(lib,"Ws2_32.lib")#pragma comment(lib,"Mswsock.lib")#pragma comment(lib,"AdvApi32.lib")int main(){	return 0;}
