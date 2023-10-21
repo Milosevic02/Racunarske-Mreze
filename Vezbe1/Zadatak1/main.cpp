b8db268d04e60b9399e1f8817edd62e14abc529c
Dragan <milosevicdragan002@gmail.com>
2023-10-21 08:37:12 +0200

    allow multiple checks and hande 'q' exit

--- Vezbe1/Zadatak1/main.cpp
+++ Vezbe1/Zadatak1/main.cpp
@@ -7,14 +7,23 @@ int main()
 	char str[1000], ch;
 	int i = 0, frequency = 0;
 	printf("Enter a string: ");
-		gets_s(str,999);
-	printf("Enter a character to find the frequency: ");
-	scanf("%c", &ch);
-	for (; str[i] != '\0'; ++i)
-	{
-		if (ch == str[i])
-			++frequency;
+	gets_s(str,999);
+	while (true) {
+		printf("Enter a character to find the frequency: ");
+		scanf("%c", &ch);
+		getchar();
+		if (ch == 'q') {
+			break;
+		}
+		for (i=0; str[i] != '\0'; ++i)
+		{
+			if (ch == str[i])
+				++frequency;
+		}
+		printf("Frequency of %c = %d", ch, frequency);
+		frequency = 0;
+
 	}
-	printf("Frequency of %c = %d", ch, frequency);
+
 	return 0;
 }
